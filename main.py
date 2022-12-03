d5c57440214a45759e68c2c3ed49127f33c89114
G-GREGOR-dev <goshasalikov@gmail.com>
2022-12-03 05:50:22 +0300

    ok

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import sys
 
-from PyQt5 import uic  # Импортируем uic
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+from random import randrange
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('01.ui', self)  # Загружаем дизайн
-        self.pushButton.clicked.connect(self.run)
-        # Обратите внимание: имя элемента такое же как в QTDesigner
+        uic.loadUi('01.ui', self)
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
 
-    def run(self):
-        self.label.setText("OK")
-        # Имя элемента совпадает с objectName в QTDesigner
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        qp.setPen(QColor(0, 0, 0, 0))
+        x = randrange(250)
+        qp.drawEllipse(randrange(500), randrange(500), x, x)
 
 
 if __name__ == '__main__':
